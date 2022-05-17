422a7fcc9001f678876066986b4dafad32b242b6
Nara Costa <naracostaaaa@gmail.com>
2022-05-17 11:27:24 -0300

    updated

--- HTTPX/Asynchronous_Programming.py
+++ HTTPX/Asynchronous_Programming.py
@@ -1,15 +1,65 @@
+## Fazendo uma solicitação HTTP com HTTPX:
 import asyncio
 import httpx
 
-
 async def main():
-    pokemon_url = 'https://pokeapi.co/api/v2/pokemon/151'
+    pokemon_url = 'https://pokeapi.co/api/v2/pokemon-species/aegislash'
 
     async with httpx.AsyncClient() as client:
-
         resp = await client.get(pokemon_url)
-
         pokemon = resp.json()
         print(pokemon['name'])
 
 asyncio.run(main())
+
+## ========================================================================
+## Fazendo um grande número de solicitações:
+import asyncio
+import httpx
+import time
+
+start_time = time.time()
+
+async def main():
+
+    async with httpx.AsyncClient() as client:
+
+        for number in range(1, 151):
+            pokemon_url = f'https://pokeapi.co/api/v2/pokemon/{number}' #vai sempre mudar o valor do numero até o 151
+
+            resp = await client.get(pokemon_url)
+            pokemon = resp.json()
+            print(pokemon['name'])
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time)) #vai mostrar o tempo para o programa rodar
+
+## ========================================================================
+## Utilizando o assíncrono para melhorar o desempenho
+
+import asyncio
+import httpx
+import time
+
+start_time = time.time()
+
+async def get_pokemon(client, url):
+        resp = await client.get(url)
+        pokemon = resp.json()
+        return pokemon['name']
+
+async def main():
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        lista_pokemon = []
+
+        for number in range(1, 20):
+            url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+            tasks.append(asyncio.ensure_future(get_pokemon(client, url)))
+
+        original_pokemon = await asyncio.gather(*tasks)
+        for pokemon in original_pokemon:
+            print(pokemon)
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time))
--- HTTPX/Context_Managers.py
+++ HTTPX/Context_Managers.py
@@ -1,4 +1,13 @@
- 
+file = open('new.txt', 'w') 
+file.write('hello')
+file.close() #vai fechar o arquivo ('isso poupa memória, caso vc abra outros arquivos')
+
+# a questão acima pode ser claramente refeita com o ContextManager:
+with open('new.txt', 'w') as file:
+        file.write('hello')
+
+
+##Outras formas de fazer:
 # ContextManager 
 class ContextManager():
     def __init__(self):
@@ -9,6 +18,7 @@ class ContextManager():
         return self
      
     def __exit__(self, exc_type, exc_value, exc_traceback):
+       # print(f"{exc_type},{exc_value},{exc_traceback}")
         print('exit method called')
 
 with ContextManager() as manager:
@@ -37,6 +47,6 @@ with Open_File('new.txt', 'w') as f:
     f.write('Testing')
     print('with statement block')
 
-print(f.closed)  #vai motrar que o programa começou, seguiu a sua ordem e por fim chegou no seu fim, que é o exit
+print(f.closed)  #após todo o programa ter sido executado, esse comando vai fechar o programa
 
 #  ===============================================================
