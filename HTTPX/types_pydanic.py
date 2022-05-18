aab9343670f644176b90c6c7a0e9c9418a4aac01
Nara Costa <naracostaaaa@gmail.com>
2022-05-18 11:49:17 -0300

    updated

---
+++ HTTPX/types_pydanic.py
@@ -0,0 +1,11 @@
+from tokenize import String
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name = 'John Doe'
+    signup_ts: Optional[datetime] = None
+    friends: List[int] = []
